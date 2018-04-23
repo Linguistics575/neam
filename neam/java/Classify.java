39c3a04088150b94cc71a845a20aa2bac1cbaca2
adoxography <gstill@uw.edu>
2018-04-23 15:46:16 -0700

    Fix paths

--- neam/java/Classify.java
+++ neam/java/Classify.java
@@ -5,30 +5,26 @@ import java.util.*;
  * Classifies a document using a NEAMClassifier
  */
 public class Classify {
-    /**
-     * The file where the Stanford CoreNLP properties are located
-     */
-    public static final String PROPERTY_FILE = "ner.prop";
-
-    public static final String TAG_FILE = "tags.prop";
-
     public static void main(String[] args) {
         // The name of the file to classify
         String fileName = args[0];
-
+        // The properties to load into the classifier
+        String properties = args[1];
+        // The tags to look for
+        String tags = args[2];
         // The NER model to use - default to null
-        String model = args.length > 1 ? args[1] : null;
+        String model = args.length > 3 ? args[3] : null;
 
         // Generate the properties
         NEAMDict props = new NEAMDict();
-        props.loadFile(PROPERTY_FILE);
+        props.loadFile(properties);
         if (model != null && model.length() > 0) {
             props.setProperty("ner.model", model);
         }
 
         // Generate the classifier
         NEAMClassifier classifier = new NEAMClassifier(props);
-        classifier.tags.loadFile(TAG_FILE);
+        classifier.tags.loadFile(tags);
 
         // Classify the document
         String tagged = classifier.classify(fileName);
--- pipeline.sh
+++ pipeline.sh
@@ -3,5 +3,8 @@
 # from https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-"$DIR/neam/java/run" "$1" "$2" | "$DIR/neam/postprocess.py" | "$DIR/neam/beautify.py"
+PROPERTY_FILE="$DIR/ner.prop"
+TAG_FILE="$DIR/tags.prop"
+
+"$DIR/neam/java/run" "$1" $PROPERTY_FILE $TAG_FILE "$2" | "$DIR/neam/postprocess.py" | "$DIR/neam/beautify.py"
 
