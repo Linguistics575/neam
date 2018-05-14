a0fc1f949cb69be9e2131e72f3f0f48da24b355c
Adoxography <gstill@uw.edu>
2018-05-14 05:36:43 -0700

    Check for and download Python dependencies on first run

--- neam.py
+++ neam.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+import sys
+import nltk
+
+try:
+    nltk.tokenize.word_tokenize('Good to go?')
+except LookupError:
+    print("Downloading NLTK models...", file=sys.stderr)
+    for data in ['punkt', 'averaged_perceptron_tagger', 'universal_tagset']:
+        print("Downloading {}...".format(data), file=sys.stderr)
+        nltk.download(data)
+
 from neam.python.neam import main
 
 main()
