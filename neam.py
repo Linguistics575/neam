782929e9657f003b04c7b636aa16948c1922a667
Adoxography <gstill@uw.edu>
2018-08-26 23:36:47 -0500

    Refactor initial NLTK downloads

--- neam.py
+++ neam.py
@@ -1,30 +1,71 @@
 #!/usr/bin/python3
+from neam.python.neam import main as run_neam
+
 import nltk
 
+from contextlib import contextmanager
 import ssl
 import sys
 
-try:
-    nltk.tokenize.word_tokenize('Good to go?')
-except LookupError:
-    """
-    From https://stackoverflow.com/a/41351871 - potentially unsafe, but should
-    work as a workaround for now where SSL checking is failing.
+NEEDED_COLLECTIONS = {
+    'tokenizers': ['punkt'],
+    'taggers': ['averaged_perceptron_tagger', 'universal_tagset']
+}
 
-    Also see https://www.python.org/dev/peps/pep-0476/
+
+def main():
+    """
+    Makes sure the NLTK dependencies have been downloaded, then passes off to
+    the real NEAM script
+    """
+    ensure_collections_exist(NEEDED_COLLECTIONS)
+    run_neam()
+
+
+def ensure_collections_exist(collections):
+    """
+    Ensures that all required collections exist on the system, and downloads
+    them if they are not present
+
+    :param collections: The collections that must be present - a dict of
+                        folder/name pairs, corresponding to where the
+                        collections should exist in the filesystem
+    :see: http://www.nltk.org/_modules/nltk/downloader.html
+    """
+    missing_collections = []
+
+    for collection_type, collection_names in collections.items():
+        for collection in collection_names:
+            try:
+                nltk.data.find(f"{collection_type}/{collection}")
+            except LookupError:
+                missing_collections.append(collection)
+
+    if missing_collections:
+        print("Downloading NLTK models...", file=sys.stderr)
+        with unverified_https_context():
+            for collection in missing_collections:
+                print("Downloading {}...".format(collection), file=sys.stderr)
+                nltk.download(collection)
+
+
+@contextmanager
+def unverified_https_context():
+    """
+    Turns off the SSL check for the duration of the function
     """
     try:
-        _create_unverified_https_context = ssl._create_unverified_context
+        create_unverified_https_context = ssl._create_unverified_context
+        create_default_https_context = ssl.create_default_https_context
     except AttributeError:
         pass
     else:
-        ssl._create_default_https_context = _create_unverified_https_context
+        ssl._create_default_https_context = create_unverified_https_context
 
-    print("Downloading NLTK models...", file=sys.stderr)
-    for data in ['punkt', 'averaged_perceptron_tagger', 'universal_tagset']:
-        print("Downloading {}...".format(data), file=sys.stderr)
-        nltk.download(data)
+    try:
+        yield
+    finally:
+        ssl._create_default_https_context = create_default_https_context
 
-from neam.python.neam import main
 
 main()
